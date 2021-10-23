68ca398acde4a739095dc4fb2a58363151c0bd2c
toilabaodai123 <74287026+toilabaodai123@users.noreply.github.com>
2021-10-23 18:43:50 +0700

    thêm đếm view

---
+++ database/migrations/2021_10_23_114118_create_visits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+			$table->string('ip');
+			$table->unsignedbiginteger('product_id')->nullable();
+			$table->integer('view_type');
+            $table->timestamps();
+			
+			$table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
