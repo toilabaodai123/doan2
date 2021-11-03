ee008d52c1eca76503f3923ae7051e937a38a642
NgocThinh21 <luungocthinh2000@gmail.com>
2021-11-03 17:14:03 +0700

    about

--- resources/views/livewire/frontend/about.blade.php
+++ resources/views/livewire/frontend/about.blade.php
@@ -24,7 +24,19 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="about__pic">
-                        <img src="img/about/about-us.jpg" alt="">
+                    <div class="testimonial__text" style="text-align: center;padding: 0 150px 0;">
+                        <div class="testimonial__author__text" >
+                            <h5> Về YaYa</h5>
+                        </div>
+                       Yaya chuyên kinh doanh các sản phẩm thời trang.
+                       Xuyên suốt quá trình hình thành và phát triển, chúng tôi 
+                       luôn tự nhận cho mình sứ mệnh tìm ra con đường ngắn nhất và tốt nhất để đưa các 
+                       doanh nghiệp tiếp cận với công nghệ hiện đại. Chúng tôi đang nỗ lực tạo ra một môi
+                        trường cạnh tranh công bằng và lành mạnh. Ở đó cơ hội cho mỗi chúng ta là như nhau, 
+                        sự phát triển của bạn phụ thuộc hoàn toàn vào lượng chất xám mà bạn có… 
+                       và chúng tôi ở đây để cung cấp cho các bạn những san phẩm thời trang hợp mode nhất. 
+                    </div>
+                </div>
                     </div>
                 </div>
             </div>
@@ -37,17 +49,12 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-6 p-0">
-                    <div class="testimonial__text">
-                        <div class="testimonial__author__text">
-                                    <h5> Về YaYa</h5>
-                        </div>
-                       Yaya chuyên kinh doanh các sản phẩm thời trang.
-                       Xuyên suốt quá trình hình thành và phát triển, chúng tôi 
-                       luôn tự nhận cho mình sứ mệnh tìm ra con đường ngắn nhất và tốt nhất để đưa các 
-                       doanh nghiệp tiếp cận với công nghệ hiện đại. Chúng tôi đang nỗ lực tạo ra một môi
-                        trường cạnh tranh công bằng và lành mạnh. Ở đó cơ hội cho mỗi chúng ta là như nhau, 
-                        sự phát triển của bạn phụ thuộc hoàn toàn vào lượng chất xám mà bạn có… 
-                       và chúng tôi ở đây để cung cấp cho các bạn những san phẩm thời trang hợp mode nhất. 
+                <div class="testimonial__text">
+                        <span class="icon_quotations"></span>
+                        <p>“Chỉ vì bạn đang khó khăn, không nghĩa là bạn đang thất bại. Mỗi thành công lớn đều đòi hỏi những thất bại cụ thể. Nếu đơn giản trở thành doanh nhân và không gặp phải bất cứ chông gai gì thì ắt hẳn ai cũng biến mình thành doanh nhân.
+Do đó, nếu như gặp khó khăn, hãy tranh đấu và đừng bỏ cuộc, bắt đầu tiến về phía trước cho đến khi mà bạn nhìn thấy ánh sáng cuối đường hầm.”
+                        </p>
+                       
                     </div>
                 </div>
                 <div class="col-lg-6 p-0">
